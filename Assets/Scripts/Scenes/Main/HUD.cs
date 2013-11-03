a5c0a956be1942082eca5ac2b9d34901b7100653
Jarek Wawro <jarek.wawro@gmail.com>
2013-11-03 13:08:18 +0100

    UIToolkit - for one draw call generation added

---
+++ Assets/Scripts/Scenes/Main/HUD.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var mainMenuButton = UIButton.create("playButton.png",
+			"playButton.png", 0 , 0 );
+	}
+
+}
